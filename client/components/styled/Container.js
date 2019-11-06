ade5ecd0ddad7b97bc6cba0c31dad685232e6f85
pbasiak <wilczek241@gmail.com>
2019-11-06 17:32:54 +0100

    Change container width

--- client/components/styled/Container.js
+++ client/components/styled/Container.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    max-width: 1000px;
+    max-width: 1100px;
     margin: 0 auto;
     ${props => props.align === 'center' ? 'text-align: center;' : undefined}
 `;
