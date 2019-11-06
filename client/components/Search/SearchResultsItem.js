0c47d8212d8569b8bc00b747bf05f662586ba438
pbasiak <wilczek241@gmail.com>
2019-11-06 17:20:32 +0100

    Add copy to clipboard - image url

--- client/components/Search/SearchResultsItem.js
+++ client/components/Search/SearchResultsItem.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { SSearchResultsImage } from './styled/SSearchResultsImage';
+import { Growler } from '../styled/Growler';
 
 export default function SearchResultItem(props) {
 
     const { imageUrl } = props;
+    const [isCopied, setIsCopied] = React.useState(false);
+
+    const onCopy = () => {
+        setIsCopied(true);
+
+        setInterval(() => {
+            setIsCopied(false);
+        }, 1500);
+    }
 
     return (
         <React.Fragment>
-            <SSearchResultsImage src={imageUrl} alt=""/>
+            <CopyToClipboard text={imageUrl} onCopy={onCopy}>
+                <SSearchResultsImage src={imageUrl} alt="" />
+            </CopyToClipboard>
+
+            {
+                isCopied && <Growler>Copied URL!</Growler>
+            }
         </React.Fragment>
     );
 }
---
+++ client/components/styled/Growler.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Growler = styled.div`
+    color: #FFF;
+    border: 2px solid #000;
+    background: #000;
+    padding: 5px 10px;
+    cursor: pointer;
+    box-shadow: 7px 5px 0 0 lightyellow;
+    transition: 0.4s all ease;
+    position: fixed;
+    right: 20px;
+    top: 20px;
+`;
